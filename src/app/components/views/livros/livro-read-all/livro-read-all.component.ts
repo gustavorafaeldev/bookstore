28a1e3fb7e1f0ad654ea4118ea2ab6cee61ec202
gustavorafaeldev <grlrafael@outlook.com>
2022-09-26 14:30:36 -0300

    deploy

---
+++ src/app/components/views/livros/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,42 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { LivroService } from './../livro.service';
+import { Livro } from './../livro.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css']
+})
+export class LivroReadAllComponent implements OnInit {
+
+  livros: Livro[] = [];
+
+  displayedColumns: string[] = ["id", "titulo", "livros", "acoes"]
+
+  id_cat: String = ''
+
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
+      this.livros = resposta
+      console.log(this.livros)
+    }, err => {
+      console.log(err)
+    })
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`])
+  }
+}
